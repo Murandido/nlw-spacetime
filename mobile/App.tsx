ba96b2ad6c7f50475b49b4384e89f5bd723aa726
Murandido <murilo.candidomc97@gmail.com>
2023-05-18 17:35:57 -0300

    stripes mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, View } from 'react-native'
+import { ImageBackground } from 'react-native'
 
 import {
   useFonts,
@@ -10,6 +10,10 @@ import {
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -28,6 +32,8 @@ export default function App() {
       className="relative flex-1 items-center bg-gray-900"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
+      <StyledStripes className="absolute left-2" />
+
       <StatusBar style="light" />
     </ImageBackground>
   )
